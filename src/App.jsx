cc7f69ee51be5bf46b08c4088d84dc57187f5086
Abhishek Sinha <v-abhissinha@microsoft.com>
2024-08-31 03:47:16 +0530

    Initial setup for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
+import { BrowserRouter } from "react-router-dom"
+
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components'
 
 function App() {
 
   return (
-    <>
-     <h1>Hello</h1>
-    </>
+     <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+           <div className="bg-hero-patter bg-cover bg-no-repeat bg-center">
+              <Navbar />
+              <Hero />
+           </div>
+           <About />
+           <Experience />
+           <Tech />
+           <Works />
+           <Feedbacks />
+           <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+           </div>
+        </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+
+import {styles} from '../style'
+import { navLinks} from '../constants/constants'
+import { logo, menu, close} from '../assets'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary `}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to ='/' className='flex items-center gap-2' onClick={()=> {setActive(""); window.scrollTo(0,0)}}>
+          <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Abhishek Sinha </p>
+        </Link>
+        <ul className='list-none hidden sm:flex felex-row gap-10'>
+            {navLinks.map((nav)=> (
+              <li key={nav.id} 
+              className= 'hover:text-white text-[20px] font-medium cursor-pointer'
+              //{`${active === nav.id ? 'text-white' : 'text-secondary'}`}
+              onClick={()=> setActive(nav, title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </nav>
   )
 }
 
