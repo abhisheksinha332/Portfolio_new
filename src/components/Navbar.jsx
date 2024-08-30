cf023ac46dbc1e5922449a191554a5e678acce47
Abhishek Sinha <v-abhissinha@microsoft.com>
2024-08-31 04:13:29 +0530

    Fixed navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { navLinks} from '../constants/constants'
 import { logo, menu, close} from '../assets'
 
 const Navbar = () => {
+  const [active, setActive] = useState(null); 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary `}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
@@ -14,15 +15,15 @@ const Navbar = () => {
           <p className='text-white text-[18px] font-bold cursor-pointer'>Abhishek Sinha </p>
         </Link>
         <ul className='list-none hidden sm:flex felex-row gap-10'>
-            {navLinks.map((nav)=> (
-              <li key={nav.id} 
-              className= 'hover:text-white text-[20px] font-medium cursor-pointer'
-              //{`${active === nav.id ? 'text-white' : 'text-secondary'}`}
-              onClick={()=> setActive(nav, title)}
-              >
-                <a href={`#${nav.id}`}>{nav.title}</a>
-              </li>
-            ))}
+        {navLinks.map((nav) => (
+          <li 
+            key={nav.id} 
+            className={`hover:text-white text-[20px] font-medium cursor-pointer ${active === nav.id ? 'text-white' : 'text-secondary'}`}
+            onClick={() => setActive(nav.id, nav.title)} 
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
         </ul>
       </div>
     </nav>
